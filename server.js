051b1f84657fca045a3161d159839d6622208275
MOHAMMADSHAIBAZQAISAR <mohammad.shaibaz.qaisar4@gmail.com>
2023-09-14 13:44:42 +0530

    soln

--- server.js
+++ server.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const app = express();
-
-// const PORT = process.env.PORT || 8080;
-
-// const baseUrl = "/calculator";
-
-// app.use(express.json());
-
-// const baseRouter = express.Router();
-
-// baseRouter.get("/greeting", (req, res) => {
-//   return res.send("Hello World!");
-// });
-
-// baseRouter.post("/add", (req, res) => {
-//   const { first, second } = req.body;
-//   res.json({ result: first + second });
-// });
-
-// baseRouter.post("/subtract", (req, res) => {
-//   const { first, second } = req.body;
-//   res.json({ result: first - second });
-// });
-
-// app.use(baseUrl, baseRouter);
-// app.listen(PORT, () => {
-//   console.log("Server running at PORT", PORT);
-// });
-
 const express = require("express");
 const app = express();
 
@@ -39,33 +9,18 @@ app.use(express.json());
 
 const baseRouter = express.Router();
 
-// Endpoint to check if the service is available
 baseRouter.get("/greeting", (req, res) => {
-  return res.status(200).json({ message: "hello world!" });
+  return res.send("Hello World!");
 });
 
-// Endpoint to add two numbers
 baseRouter.post("/add", (req, res) => {
   const { first, second } = req.body;
-  if (typeof first !== "number" || typeof second !== "number") {
-    return res
-      .status(400)
-      .json({ error: "Both 'first' and 'second' must be numbers" });
-  }
-  const result = first + second;
-  res.status(200).json({ result });
+  res.json({ result: first + second });
 });
 
-// Endpoint to subtract two numbers
 baseRouter.post("/subtract", (req, res) => {
   const { first, second } = req.body;
-  if (typeof first !== "number" || typeof second !== "number") {
-    return res
-      .status(400)
-      .json({ error: "Both 'first' and 'second' must be numbers" });
-  }
-  const result = first - second;
-  res.status(200).json({ result });
+  res.json({ result: first - second });
 });
 
 app.use(baseUrl, baseRouter);
